35a1f3b0cdeedae27a3a0acf61290bcb31e0f1a3
Cauê Morenghi <126644843+cauemorenghi14@users.noreply.github.com>
2023-05-26 17:06:12 -0300

    Delete javascript directory

--- javascript/register.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var form = document.querySelector('.formulario');
-
-    form.addEventListener('submit', (evento) => {
-        evento.preventDefault();
-
-        window.location.href = '../html/login.html'
-    })
-})
